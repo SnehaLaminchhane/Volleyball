8e00f055e6c52d95f0b6d96c793444601c33135d
SnehaLaminchhane <135504466+SnehaLaminchhane@users.noreply.github.com>
2023-08-12 18:42:26 +1000

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,200 @@
+function Home() {
+    return (
+        <>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+            <div className="navbar">
+                <a href="#home">Home</a>
+                <a href="#av">About Volleyball</a>
+                <a href="#sneha">Contact</a>
+                <a href="#people">People</a>
+                <a href="#rules">Rules</a>
+            </div>
+            <div id="home">
+            <marquee><h1>Volleyball</h1></marquee>
+            </div>
+            <br />
+            <hr />
+            <div id="av" className="container-fluid">
+                <h3>About Volleyball</h3>
+            <p>Volleyball is a team sport in which two teams of six players are separated by a net. Each team tries to score points by grounding a ball on the other team's court under organized rules. It has been a part of the official program of the Summer Olympic Games since Tokyo 1964. Beach volleyball was introduced to the programme at the Atlanta 1996 Summer Olympics. The adapted version of volleyball at the Summer Paralympic Games is sitting volleyball.</p>
+            </div>
+            <hr />
+            <br />
+            <center>
+                <img src="https://i.gifer.com/JGB.gif" />
+            </center>
+            <br />
+            <hr />
+            <p>The seven positions in volleyball are outside hitter, opposite, setter, middle blocker, libero, defensive specialist, and serving specialist.</p>
+            <hr />
+            <img src="https://volleyball-tips.com/wp-content/uploads/2018/07/volleyball-positions-free-e1530899115728.jpg" />
+            <hr />
+            <br />
+            <div className="card-group">
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://media.tenor.com/F4j0PdvYf4cAAAAC/libero-volleyball.gif" height="250" width="300" />
+                        <h3 className="text-center">Libero</h3>
+                        <p>A libero in indoor volleyball is a back-row defensive specialist. Since they only play in the back row, those players are often shorter than the front-row blockers and hitters but have impeccable ball-control skills.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://thumbs.gfycat.com/AbsoluteTeemingDunnart-size_restricted.gif" height="250" width="300" />
+                        <h3 className="text-center">Outside hitter</h3>
+                        <p>In a 6-1, an outside hitter has the option to play all the way around, passing, playing defense, and hitting out of the back row. An opposite's responsibilities include swinging from the front and back row, playing right-back defense, and blocking the other team's outside hitter.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://volleyballvault.com/wp-content/uploads/2022/05/leal.gif" height="250" width="300" />
+                        <h3 className="text-center">Opposite</h3>
+                        <p>An opposite has the option to play all the way around, passing, playing defense, and hitting out of the back row. An opposite's responsibilities include swinging from the front and back row, playing right-back defense, and blocking the other team's outside hitter.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://thumbs.gfycat.com/AnyLankyDeermouse-max-1mb.gif" height="250" width="300" />
+                        <h3 className="text-center">Setter</h3>
+                        <p>The Setter in Volleyball is the player responsible for strategizing the team's attack plays in Volleyball by using the Set.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-group">
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://media.tenor.com/H1P4kwQdYusAAAAC/haikyuu-block.gif" height="250" width="300" />
+                        <h3 className="text-center">Middle Blocker</h3>
+                        <p> In volleyball, a middle blocker is a player who typically plays near the net and is responsible for blocking shots from the opposing team's</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://media4.giphy.com/media/ifeE2Q94rejQ1UTdrR/giphy.gif?cid=82a1493b2kiot4wbbj5q7r0la0pszt4b01lw2xm7mnzedkms&ep=v1_gifs_search&rid=giphy.gif&ct=g" height="250" width="300" />
+                        <h3 className="text-center">Defensive Specialist</h3>
+                        <p>A defensive specialist in volleyball is a specific role separate from the libero belonging to an experienced player on the team proficient in defense technique and strategy. </p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://preview.redd.it/ltpbbx3l1sn71.gif?width=600&auto=webp&s=68840cb6c30a67f7e669cc53658d2a42c06a97a6" height="250" width="300" />
+                        <h3 className="text-center">Serving Specialist (SS)</h3>
+                        <p>A serving specialist is a player who subs in just to serve. These players typically have a very tough or very consistent serve, and they come in for a player who is less strong behind the service line, but otherwise very valuable to have on the court as an offensive or defensive threat.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <img src="https://i.pinimg.com/736x/4e/f9/b6/4ef9b636951b955350da1bbe74089e32.jpg" height="250" width="300" />
+                        <h1 className="text-center">VolleyBall Positions!</h1>
+                    </div>
+                </div>
+            </div>
+            <div id="people">
+            <div class="container">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                        <li data-target="#myCarousel" data-slide-to="4"></li>
+                        <li data-target="#myCarousel" data-slide-to="5"></li>
+                        <li data-target="#myCarousel" data-slide-to="6"></li>
+                        <li data-target="#myCarousel" data-slide-to="7"></li>
+                        <li data-target="#myCarousel" data-slide-to="8"></li>
+                    </ol>
+
+
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="https://i.ytimg.com/vi/gnljkD-rAZo/maxresdefault.jpg"  />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://news.cgtn.com/news/3d3d414e3345444d78457a6333566d54/img/7e639afa58f646eca95b164337568b8a/7e639afa58f646eca95b164337568b8a.jpg"  />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://i.hurimg.com/i/hdn/75/600x369/6072a25618c7733aac235746.jpg" />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://pbs.twimg.com/media/EIimVXYUEAIF5aI?format=jpg&name=4096x4096"  />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://www.fivb.org/Vis2009/Images/GetImage.asmx?Type=Press&No=80921&width=300&height=450&stretch=uniformtofill"  />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://volleyverse.com/wp-content/uploads/2021/12/kim-yeon-koung-450.png"  />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://volleybox.net/media/upload/post_images/1672467916Jes9n.jpg"  />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://cdn.imago-images.com/bild/sp/0046082003/m.jpg" />
+                        </div>
+
+                        <div class="item">
+                            <img src="https://volleybox.net/media/upload/players/1688198844JdeK1.png" />
+                        </div>
+                    </div>
+
+
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            </div>
+            <p> The best Volleyball players consist of...</p>
+            <ol>
+                <li>Ekaterina Antropova Opposite from Italy</li>
+                <li>Ting Zhu Outside Hitter from China.</li>
+                <li>Melissa Vargas Abreu Opposite from Cuba.</li>
+                <li>Tessa Grubbs Opposite from USA.</li>
+                <li>Sarina Nishida Outside Hitter from Japan.</li>
+                <li>Yeon-Koung Kim Outside Hitter from South Korea.</li>
+                <li>Ayaka Araki Middle-blocker from Japan.</li>
+                <li>Elizabet Inneh-Varga Opposite from Romania.</li>
+            </ol>
+            
+                <div id="rules">
+                    
+                    <h2>Basic Rules of Volleyball</h2>
+                    <img className="card-img-top img-circle" src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3Y2NWlkOXBsdnMwYmtzMmt5NnF1OXQ0cWlqa284YnFjOHVteGVjdCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/W02lJpO1pd0BpRv8YM/giphy.gif" height="300px" width="200px"/>
+                    <p>6 players on a team, 3 on the front row and 3 on the back row. There is a maximum of three hits per side. Player may not hit the ball twice in succession (A block is not considered a hit) Ball may be played off the net during a volley and on a serve. A ball hitting a boundary line is 'in'</p>
+                </div>
+               
+                <div id="sneha">
+            <footer>
+                <h3>Contact Us @</h3>
+                <ul id="contact">
+                    <li>Phone No.: 0401453076</li>
+                    <li>Email: Volleyball@email.com</li>
+                    <li>FaceBook: VolleyBallOfficial</li>
+                    <li>Twitter: VolleyBallOfficial</li>
+                    <li>Instagram: VolleyBallOfficial</li>
+                </ul>
+            </footer>
+            </div>
+        </>
+    )
+}
+export default Home;
---
+++ index.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+import Home from "./Home";
+import Form from "./Form";
+ReactDOM.render(
+    <React.Fragment>
+        <Home/>
+        <Form/>
+    </React.Fragment>
+    
+    ,document.getElementById("root"));
